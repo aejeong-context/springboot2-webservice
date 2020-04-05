e05a3cd03f78519a31e6278d69824e886e2de73e
aejeong <aejeong-context@kakao.com>
2020-04-05 11:54:44 +0900

    How to use lombok

---
+++ src/main/java/com/jeong/app/springboot/web/Dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.jeong.app.springboot.web.Dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
