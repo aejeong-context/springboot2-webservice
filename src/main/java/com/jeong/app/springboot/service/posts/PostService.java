64d7671797a105cd104b1b3fd5462fefa5edc69e
aejeong <aejeong-context@kakao.com>
2020-04-10 17:13:04 +0900

    add update(Dirty Checking) API

--- src/main/java/com/jeong/app/springboot/service/posts/PostService.java
+++ src/main/java/com/jeong/app/springboot/service/posts/PostService.java
@@ -1,7 +1,11 @@
 package com.jeong.app.springboot.service.posts;
 
 import com.jeong.app.springboot.domain.posts.PostRepository;
+import com.jeong.app.springboot.domain.posts.Posts;
+import com.jeong.app.springboot.web.dto.PostsResponseDto;
 import com.jeong.app.springboot.web.dto.PostsSaveRequestDto;
+import com.jeong.app.springboot.web.dto.PostsUpdateRequestDto;
+import javafx.geometry.Pos;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +18,17 @@ public class PostService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+    //더티체킹 - 트랜잭션이 끝나는 시점에 변화가 있는 모든 엔티티 객체를 DB에 자동으로 반영
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+        return id;
+    }
+    @Transactional
+    public PostsResponseDto findByID(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다.id="+id));
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/com/jeong/app/springboot/web/PostsApiController.java
+++ src/main/java/com/jeong/app/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.jeong.app.springboot.web;
 
 import com.jeong.app.springboot.service.posts.PostService;
+import com.jeong.app.springboot.web.dto.PostsResponseDto;
 import com.jeong.app.springboot.web.dto.PostsSaveRequestDto;
+import com.jeong.app.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //Bean 주입 방식 Autowired , setter, 생성자 
 // 생성자 권장 - > RequiredArgsConstructor에서 final이 선언된 모든 필드를 인자값으로 하는 생성자를 대신 생성
@@ -14,9 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PostsApiController {
     private final PostService postService;
+    //등록
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postService.save(requestDto);
+    }
+    //수정
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id,@RequestBody PostsUpdateRequestDto requestDto){
+        return postService.update(id,requestDto);
+    }
+    //조회
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postService.findByID(id);
     }
 
 }
