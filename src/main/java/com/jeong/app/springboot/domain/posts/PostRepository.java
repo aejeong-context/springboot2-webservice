2b623d61eccf7278934c618813b435c5ceb1393d
aejeong <aejeong-context@kakao.com>
2020-04-12 21:58:47 +0900

    mustache를 이용한 전체조회 화면

--- src/main/java/com/jeong/app/springboot/domain/posts/PostRepository.java
+++ src/main/java/com/jeong/app/springboot/domain/posts/PostRepository.java
@@ -1,7 +1,13 @@
 package com.jeong.app.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 //Dao와 같은 DB Layer접근자. JAP에서는 Repository라고 부르며 인터페이스로 생성한다.
 //JapRepository<Entity클래스,PK타입> 을 상속하면 기본적인 CRUD메소드가 자동으로 생성된다.
 public interface PostRepository extends JpaRepository<Posts,Long> {
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jeong/app/springboot/service/posts/PostService.java
+++ src/main/java/com/jeong/app/springboot/service/posts/PostService.java
@@ -2,14 +2,16 @@ package com.jeong.app.springboot.service.posts;
 
 import com.jeong.app.springboot.domain.posts.PostRepository;
 import com.jeong.app.springboot.domain.posts.Posts;
+import com.jeong.app.springboot.web.dto.PostsListResponseDto;
 import com.jeong.app.springboot.web.dto.PostsResponseDto;
 import com.jeong.app.springboot.web.dto.PostsSaveRequestDto;
 import com.jeong.app.springboot.web.dto.PostsUpdateRequestDto;
-import javafx.geometry.Pos;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +32,11 @@ public class PostService {
     public PostsResponseDto findByID(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다.id="+id));
         return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly = true)
+        public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
